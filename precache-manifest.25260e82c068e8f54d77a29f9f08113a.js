0caeb54c020a5577e4722b5fedb60488e34752c1
randyemens <randy.emens@gmail.com>
2020-04-18 11:30:52 -0700

    Updates

---
+++ precache-manifest.25260e82c068e8f54d77a29f9f08113a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751325c027a7614ce5f0110157267e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52df9381ae0559ee0714",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c452fa471f022d230d77",
+    "url": "/static/js/2.930a1725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.930a1725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52df9381ae0559ee0714",
+    "url": "/static/js/main.b8ec89d8.chunk.js"
+  },
+  {
+    "revision": "c72b664e81a03c0c7cc1",
+    "url": "/static/js/runtime-main.5a26e5ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
